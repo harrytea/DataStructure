d18e7bc116977065d164104ddd8f13ee901139b1
harry <1061771439@qq.com>
2019-04-20 10:19:37 +0800

    finish task

--- task/stack_bin_task/Huffman_code.c
+++ task/stack_bin_task/Huffman_code.c
@@ -1,32 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define NDEBUG
-#include<assert.h>
+//#define NDEBUG
+//#include<assert.h>
 #define m_m 10
 #define n_n 2*m_m-1
 typedef struct {
-	char letter, *code;
+	char letter, *code;//字符，编码
 	int node;
 	int parent;
 	int lchild;
 	int rchild;
 }HTree, Huffmantree[n_n + 1];
 int strlen(char* str) {//求字符串的长度
-	assert(str != NULL);
+	//assert(str != NULL);
 	int len = 0;
 	while (*str++ != '\0')
 		++len;
 	return len;
 }
-char* strcpy(char* strDes, char* strSrc) {
+/*char* strcpy(char* strDes, char* strSrc) {
 	assert((strDes != NULL) && (strSrc != NULL));
 	char* address = strDes;
-	while ((*strSrc) != '\0')
-		*strDes++ = *strSrc++;
+	while ((*strDes) != '\0')
+		*strDes++ = *strSrc;
 	return address;
-}//串复制，将src复制到des中
+}//串复制，将src复制到des中*/
 int strcmp(char* s, char* t) {
-	assert(s != NULL && t != NULL);
+	//assert(s != NULL && t != NULL);
 	while (*s&&*t&&*s == *t) {
 		++s;
 		++t;
@@ -41,7 +41,7 @@ void select(Huffmantree HT, int n, int *s1, int *s2) {
 			min = HT[i].node;
 			temp1 = i;
 		}
-	}
+	}//寻找第一个最小值
 	*s1 = temp1;
 	min = 10000;
 	for (int i = 0; i < n; i++) {
@@ -49,9 +49,9 @@ void select(Huffmantree HT, int n, int *s1, int *s2) {
 			min = HT[i].node;
 			temp2 = i;
 		}
-	}
+	}//寻找第二个最小值
 	*s2 = temp2;
-}
+}//求哈夫曼树中最小的两项以合并
 void CreatHTree(Huffmantree HT, int s[],char ss[], int n) {
 	for (int i = 0; i < n; i++) {
 		HT[i].letter = ss[i];
@@ -59,7 +59,7 @@ void CreatHTree(Huffmantree HT, int s[],char ss[], int n) {
 		HT[i].lchild = 0;
 		HT[i].rchild = 0;
 		HT[i].parent = 0;
-	}
+	}//将霍夫曼树的前n项初始化
 	int m = 2 * n - 1;
 	for (int i = n ; i < m; i++) {
 		HT[i].letter = '#';
@@ -67,7 +67,7 @@ void CreatHTree(Huffmantree HT, int s[],char ss[], int n) {
 		HT[i].lchild = 0;
 		HT[i].rchild = 0;
 		HT[i].parent = 0;
-	}
+	}//将霍夫曼的后n-1项初始化
 	int s1, s2;
 	for (int i = n ; i < m; i++) {
 		select(HT, i, &s1, &s2);
@@ -76,8 +76,8 @@ void CreatHTree(Huffmantree HT, int s[],char ss[], int n) {
 		HT[s2].parent = i;
 		HT[i].lchild = s1;
 		HT[i].rchild = s2;
-	}
-}
+	}//构建霍夫曼树
+}//初始化霍夫曼树
 void CreatHTreecode(Huffmantree HT,int n) {
 	char **cd = (char**)malloc(sizeof(char*)*n);
 	for (int i = 0; i < n; i++)
@@ -97,34 +97,33 @@ void CreatHTreecode(Huffmantree HT,int n) {
 				cd[i][start] = '1';
 			c = p;
 			p = HT[p].parent;
-		}
+		}//存放霍夫曼编码后的值
 		int j,k=0;
 		HT[i].code = (char*)malloc(sizeof(char)*n);
 		for (j = start; cd[i][j] != '\0'; j++) {
 			HT[i].code[k++] = cd[i][j];
 			printf("%c", cd[i][j]);
-		}
+		}//将霍夫曼编码后的值对应放到霍夫曼树中
 		HT[i].code[k] = '\0';
 		printf("\n");
 	}
 	return;
-}
-void HuffmanTree_decode(Huffmantree HT, char ss[], int n) {
+}//霍夫曼编码
+void HuffmanTree_decode(Huffmantree HT, char ss[], int n,int sum) {
 	char sen[100];
 	char temp[50];
-	char voidstr[] = " ";       //空白字符串
+	//char voidstr[] = " ";       //空白字符串
 	int t = 0;
 	int s = 0;
-	int xx = 0;
-	for (int i = 0; i<n; i++) {
+	int pos = 0;
+	for (int i = 0; i<sum-1; i++) {
 		temp[t++] = ss[i];     //读取字符
-		temp[t] = '\0';        //有效字符串
+		temp[t] = '\0';        //有效字符串结束
 		for (int j = 0; j<n; j++) {        //依次与所有字符编码开始匹配
-			if (!strcmp(HT[j].code, temp)) {                //匹配成功
-				sen[s] = HT[j].letter;    //将字符保存到sen中
-				s++;
-				xx += t;
-				strcpy(temp, voidstr);                //将TEMP置空
+			if (!strcmp(HT[j].code, temp)) { //匹配成功
+				sen[s++] = HT[j].letter;    //将编码对应字符保存到sen中
+				pos += t;
+				//strcpy(temp, voidstr);    //将TEMP置空
 				t = 0;          //t置空
 				break;
 			}
@@ -133,16 +132,17 @@ void HuffmanTree_decode(Huffmantree HT, char ss[], int n) {
 	if (t == 0) {     //t如果被置空了，表示都匹配出来了，打印译码
 		sen[s] = '\0';
 		puts("译码为：");
-		printf("%s", sen);
+		printf("%s\n", sen);
 	}
 	else                             //t如果没有被置空 ， 源码无法被完全匹配
-		printf("输入的二进制源码有错误，从第%d位开始", xx + 1);
-}
+		printf("输入的二进制源码有错误，从第%d位开始",pos + 1);
+}//霍夫曼解码
 int main() {
-	char str[100];
-	char ss[10];
-	int s[10],n=0,j;
-	gets_s(str);
+	char str[100];//存放要编码的字符串
+	char ss[10];//存放字符串中的统计项
+	int s[10];//存放字符串中字符的权值
+	int n=0,j;
+	gets_s(str);//输入字符串
 	int len = strlen(str);
 	for (int i = 0; i < len; i++) {
 		for (j = 0; j < n; j++) {
@@ -155,13 +155,13 @@ int main() {
 			s[n] = 1;
 			n++;
 		}
-	}
+	}//统计字符和对应的权值
 	for (int i = 0; i<n; i++){
 		printf("字符：%c，权值：%d\n", ss[i], s[i]);
 	}//打印字符及对应的权值
 	Huffmantree HT;
-	CreatHTree(HT,s,ss,n);
-	CreatHTreecode(HT, n);
+	CreatHTree(HT,s,ss,n);//构建霍夫曼树
+	CreatHTreecode(HT, n);//对霍夫曼树进行编码
 	printf("huffman树的结构为:\n");
 	for (int i = 0; i <2 * n - 1; i++)//打印huffman_tree
 		printf("%c %d %d %d %d\n",HT[i].letter, HT[i].node, HT[i].parent, HT[i].lchild, HT[i].rchild);
@@ -170,7 +170,7 @@ int main() {
 		printf("%c %s\n", HT[i].letter, HT[i].code);//输出huffman编码
 	while (1) {
 		puts("请输入译码的二进制字符串并以‘#’结束");
-		int sign = 1;//判断是否输入正确的标志
+		int sign = 1;//判断是否输入正确的字符‘1’‘0’或‘#’
 		int sum = 0;//计数输入的总字符数
 		int symbol = 1;//判断是否输入结束
 		char string[100];
@@ -182,51 +182,14 @@ int main() {
 				sign = 0;
 			if (sss == '#') {
 				symbol = 0;
-				getchar();
 			}
 		}
 		string[sum] = '\0';
 		if (sign == 1)
-			HuffmanTree_decode(HT, string, sum);
+			HuffmanTree_decode(HT, string,n, sum);
 		else
 			puts("输入有误");
+		getchar();//接受换行符
 	}
 	return 0;
 }
-/*
-void HuffmanTreeYima(HuffmanTree HT, char cod[], int b)           //译码
-{
-	char sen[100];
-	char temp[50];
-	char voidstr[] = " ";       //空白字符串
-	int t = 0;
-	int s = 0;
-	int xx = 0;
-	for (int i = 0; i<b; i++)
-	{
-		temp[t++] = cod[i];     //读取字符
-		temp[t] = '\0';        //有效字符串
-		for (int j = 0; j<n; j++) {        //依次与所有字符编码开始匹配
-
-			if (!strcmp(HT[j].code, temp)) {                //匹配成功
-
-				sen[s] = HT[j].letter;    //将字符保存到sen中
-				s++;
-				xx += t;
-				strcpy(temp, voidstr);                //将TEMP置空
-				t = 0;          //t置空
-				break;
-			}
-		}
-	}
-	if (t == 0) {     //t如果被置空了，表示都匹配出来了，打印译码
-
-		sen[s] = '\0';
-		cout << "译码为:" << endl;
-
-		cout << sen << endl;
-	}
-	else {                             //t如果没有被置空 ， 源码无法被完全匹配
-		cout << "二进制源码有错！从第" << xx + 1 << "位开始" << endl;
-	}
-}*/
--- task/stack_bin_task/wood.c
+++ task/stack_bin_task/wood.c
@@ -2,7 +2,7 @@
 #define m_m 10
 #define n_n 2*m_m-1
 typedef struct{
-	int node;
+	int node;//代表结点的木头长度
 	int parent;
 	int lchild;
 	int rchild;
@@ -15,7 +15,7 @@ void select(Huffmantree HT, int n, int *s1, int *s2) {
 			min = HT[i].node;
 			temp1 = i;
 		}
-	}
+	}//寻找第一个最小值
 	*s1 = temp1;
 	min = 1000;
 	for (int i = 1; i <= n; i++) {
@@ -23,23 +23,23 @@ void select(Huffmantree HT, int n, int *s1, int *s2) {
 			min = HT[i].node;
 			temp2 = i;
 		}
-	}
+	}//寻找第二个最小值
 	*s2 = temp2;
-}
+}//求哈夫曼树中最小的两项以合并
 void CreatHTree(Huffmantree HT, int money[], int n) {
 	for (int i = 1; i <= n; i++) {
 		HT[i].node = money[i];
 		HT[i].lchild = 0;
 		HT[i].rchild = 0;
 		HT[i].parent = 0;
-	}
+	}//初始化霍夫曼树的前n项
 	int m = 2 * n - 1;
 	for (int i = n + 1; i <= m; i++) {
 		HT[i].node = 0;
 		HT[i].lchild = 0;
 		HT[i].rchild = 0;
 		HT[i].parent = 0;
-	}
+	}//将霍夫曼树的后n-1项初始化
 	int s1, s2;
 	for (int i = n + 1; i <= m; i++) {
 		select(HT, i - 1, &s1, &s2);
@@ -48,13 +48,13 @@ void CreatHTree(Huffmantree HT, int money[], int n) {
 		HT[s2].parent = i;
 		HT[i].lchild = s1;
 		HT[i].rchild = s2;
-	}
-}
+	}//构建霍夫曼树
+}//初始化霍夫曼树
 int main() {
 	int N,money[m_m];
 	scanf("%d",&N);
 	for (int i = 1; i <= N; i++)
-		scanf("%d", &money[i]);
+		scanf("%d", &money[i]);//输入切割后每个木头的长度
 	Huffmantree HT;
 	CreatHTree(HT, money, N);
 	printf("haffman树的结构为:\n");
@@ -64,7 +64,7 @@ int main() {
 	for (int i = 2 * N - 1; i >= N + 1; i--) {
 		printf("将%d切分为%d和%d\n", HT[i].node, HT[(HT[i].lchild)].node, HT[(HT[i].rchild)].node);
 		sum += HT[i].node;
-	}
+	}//输出切割过程
 	printf("总花费为%d\n", sum);
 	return 0;
 }
